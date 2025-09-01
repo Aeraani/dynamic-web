120d5b5c2f120e3d93623e2ed78842fce4c92d2c
Park Tae Ran\taera <taeran0129@naver.com>
2025-09-01 14:56:37 +0900

    mvc

--- src/main/java/com/multi/controller/ListAction.java
+++ src/main/java/com/multi/controller/ListAction.java
@@ -2,10 +2,15 @@
 package com.multi.controller;
 
 import com.multi.util.DBUtil;
+import com.multi.vo.EmpVO;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListAction  implements Action {
     private String path;
@@ -21,7 +26,27 @@ public class ListAction  implements Action {
         //BL수행
         System.out.println("ListAction execute");
         request.setAttribute("arraylist","hello World");
-        System.out.println("connection="+ DBUtil.getConnection());
+        //System.out.println("connection="+ DBUtil.getConnection());
+        Connection connection=null; //DB 커낵션 가져옴
+        String sql="select * from emp";
+        PreparedStatement pstmt=null;
+        ResultSet resultSet=null;
+        List <EmpVO> empVOList=new ArrayList<EmpVO>();
+        try{
+            connection=DBUtil.getConnection();
+            pstmt=connection.prepareStatement(sql);
+            resultSet=pstmt.executeQuery();
+            while (resultSet.next()){ //tuple별로 데이터 가져오기
+                EmpVO empVO=new EmpVO();
+                empVO.setEmpno(resultSet.getInt("empno"));
+                empVO.setEname(resultSet.getString("ename"));
+
+            }
+        }catch(Exception e){
+
+        }finally {
+
+        }
 
         return new ActionForWard(path, isRedirect);
     }
